e606a771c5422639a9f9781b5b69dac16b34a728
Tonirayn <marino.antonio96@gmail.com>
2020-09-14 16:33:11 +0200

    submodule updates

--- MATLAB/dynopt_code/tbxmanager/toolboxes/dynopt/5.0.2/all/dynopt_code-master/examples/problem1a/objfun.m
+++
@@ -1,4 +0,0 @@
-function f = objfun(t,x,u,p)
-
-f = [x(2)];
-
--- MATLAB/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd dynopt_code/tbxmanager
-matlab -nodisplay -nodesktop -r "run tbxmanager; run savepath; tbxmanager restorepath; exit"
-cd ../..
